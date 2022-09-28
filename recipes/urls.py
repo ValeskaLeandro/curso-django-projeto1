8e593b71a932e6cd0358a608b5c4fe3197380111
Valeska Leandro <valeskaleandro@gmail.com>
2022-09-28 16:08:29 -0300

    Configurando URLs dinâmicas

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from recipes import views  # posso usar . no lugar de recipes
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),  # Home
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),  # Home
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
