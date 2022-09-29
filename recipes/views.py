59a70ba3202daaa6983b6d252768d97819081b18
Valeska Leandro <valeskaleandro@gmail.com>
2022-09-29 14:38:00 -0300

    Separando por Categorias

--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
+from recipes.models import Recipe
+
 
 # Create your views here.
 def home(request):
+    # Ordenando por ordem de cadastro invertida
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    # Ordenando por ordem de cadastro invertida
+    recipes = Recipe.objects.filter(category_id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
     })
 
 
