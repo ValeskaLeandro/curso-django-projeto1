b91e197e59d92835c69cbbf4619ca3661c10faa1
Valeska Leandro <valeskaleandro@gmail.com>
2022-10-10 16:08:19 -0300

    test_recipe_home_template_loads_recipes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -11,6 +11,15 @@ class RecipeViewsTest(RecipeTestBase):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
+    def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe()
+
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.content['recipes']
+        self.assertIn('Recipe Title', content)
+        self.assertEqual(len(response_context_recipes), 1)
+
     def test_recipe_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertEqual(response.status_code, 200)
