9ea84eb4020b0e8e7c8b96ea4aa423b588fdcc2f
Valeska Leandro <valeskaleandro@gmail.com>
2022-10-13 16:45:38 -0300

    Upgrade paginação + teste paginação

--- utils/pagination.py
+++ utils/pagination.py
@@ -31,8 +31,8 @@ def make_pagination_range(
         'total_pages': total_pages,
         'start_range': start_range,
         'stop_range': stop_range,
-        'first_page_out_of_range': 1,
-        'last_page_out_of_range': stop_range,
+        'first_page_out_of_range': current_page > middle_range,
+        'last_page_out_of_range': stop_range < total_pages,
     }
 
 
