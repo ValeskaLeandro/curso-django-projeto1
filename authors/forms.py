b3d2d37658fd2561d82306c786dd1b33b607b319
Valeska Leandro <valeskaleandro@gmail.com>
2022-10-14 16:50:08 -0300

    Upgrade no Forms

--- authors/forms.py
+++ authors/forms.py
@@ -1,10 +1,46 @@
-from tkinter import Widget
-
 from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Seu usuário')
+        add_placeholder(self.fields['email'], 'Ex: email@email.com')
+        add_placeholder(self.fields['first_name'], 'Ex.: Maria')
+        add_placeholder(self.fields['last_name'], 'Ex.: Silva')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Digite sua senha'
+        }),
+        error_messages={
+            'required': 'A senha não deve estar vazia'
+        },
+        help_text=(
+            'A senha deve ter pelo menos uma letra maiúscula, '
+            'uma letra minúscula e um número. O comprimento deve ser '
+            'pelo menos 8 caracteres.'
+        )
+    )
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repita sua senha'
+        })
+    )
+
     class Meta:
         model = User
         fields = [
@@ -14,7 +50,3 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
-
-    widgets = {
-        'password': forms.PasswordInput(attrs={'placeholder': 'Escolha sua senha.'}),
-    }
