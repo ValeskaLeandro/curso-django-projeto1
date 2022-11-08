b4971c09b139730a69526f931415e924442a59bc
Valeska Leandro <valeskaleandro@gmail.com>
2022-11-08 17:50:46 -0300

    Models Tags

--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,6 @@
+import string
+from random import SystemRandom
+
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
@@ -7,7 +10,7 @@ from django.utils.text import slugify
 class Tag(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
-
+    # Aqui começam os campo para a relação genérica
     # Representa o model que queremos encaixar aqui
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     # Representa o id da linha do model descrito acima
@@ -19,5 +22,14 @@ class Tag(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            ...
+            rand_letters = ''.join(
+                SystemRandom().choices(
+                    string.ascii_letters + string.digits,
+                    k=5,
+                )
+            )
+            self.slug = slugify(f'{self.name}-{rand_letters}')
         return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
